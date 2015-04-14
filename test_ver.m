3f4fc69a1d2ae2bd3be2a1abd52af00e905b1807
Jan Heimann <jan.heimann@student.jade-hs.de>
2015-04-14 10:04:49 +0200

    Ich habe einen  Teil auskommentiert

--- test_ver.m
+++ test_ver.m
@@ -4,14 +4,14 @@
 path = 'C:\Users\Jan Heimann\Desktop\Jade Hochschule\SoSe 15\Matlab++\Aufgabe 1\timit\TIMIT MIT';
 
 
-TextFile = 'allfilelist.txt';
-fid = fopen(TextFile);
-%scans the content of the text document into an array in column order
-xx = textscan(fid, '%s'); 
-
-%sztext1 = fprintf('%s',z);
-
-fclose(fid);
+% TextFile = 'allfilelist.txt';
+% fid = fopen(TextFile);
+% %scans the content of the text document into an array in column order
+% xx = textscan(fid, '%s'); 
+% 
+% %sztext1 = fprintf('%s',z);
+% 
+% fclose(fid);
 
 
 %%
