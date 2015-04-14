0cf90550a2535b0522684e9eb3a182b0b12d57fc
Philip Luedtke <philip.luedtke@gmx.de>
2015-04-14 13:53:40 +0200

    Ich habe mal unsere Hauptfunktion inkl. Tester erstellt. Man hat die Moeglichkeit die Anzahl der Kriterien, sowie 4 Suchkriterien in die Funktion zu übergeben. Dies ist nur das Grundgeruest

---
+++ SearchDatabase.m
@@ -0,0 +1,45 @@
+function [out_param] = SearchDatabase(NrOfCrits, Crit1, Crit 2, Crit3, Crit 4)
+% function to do something usefull (fill out)
+% Usage [out_param] = SearchDatabase(in_param)
+% Input Parameter:
+%	 NrOfCrits: 		 Number of search-criterions
+%    Crit1...4:          String wich contains the search-keyword
+% Output Parameter:
+%	 out_param:          Answer of your search
+%------------------------------------------------------------------------ 
+% Example: Provide example here if applicable (one or two lines) 
+
+% Author: J. Heimann, D. Popken, P. Luedtke (c) TGM @ Jade Hochschule applied licence see EOF 
+% Source: If the function is based on a scientific paper or a web site, 
+%         provide the citation detail here (with equation no. if applicable)  
+% Version History:
+% Ver. 0.01 initial create (empty) 14-Apr-2015  Initials (JH, DP, PL)
+
+%------------Your function implementation here--------------------------- 
+
+switch NrOfCrits
+    case '1'
+        
+        
+    case '2'
+    case '3'
+    case '4'        
+%--------------------Licence ---------------------------------------------
+% Copyright (c) <2015> J. Heilmann, D. Popken, P. Luedtke
+% Jade University of Applied Sciences 
+% Permission is hereby granted, free of charge, to any person obtaining 
+% a copy of this software and associated documentation files 
+% (the "Software"), to deal in the Software without restriction, including 
+% without limitation the rights to use, copy, modify, merge, publish, 
+% distribute, sublicense, and/or sell copies of the Software, and to
+% permit persons to whom the Software is furnished to do so, subject
+% to the following conditions:
+% The above copyright notice and this permission notice shall be included 
+% in all copies or substantial portions of the Software.
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+% IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+% CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+% TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+% SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- test_ver.m
+++ test_ver.m
@@ -1,7 +1,10 @@
 %%
+clc;
+close all;
 
 
-path = 'C:\Users\Jan Heimann\Desktop\Jade Hochschule\SoSe 15\Matlab++\Aufgabe 1\timit\TIMIT MIT';
+%path = 'C:\Users\Jan Heimann\Desktop\Jade Hochschule\SoSe 15\Matlab++\Aufgabe 1\timit\TIMIT MIT';   %Jan
+path = '/Users/andimeu/Documents/FH/6. Semester/DALGO/DALGO_PROJEKT/TIMIT MIT';   %Philip
 
 
 TextFile = 'allfilelist.txt';
@@ -12,9 +15,18 @@ xx = textscan(fid, '%s');
 %sztext1 = fprintf('%s',z);
 
 fclose(fid);
-
+info = cd
 
 %%
+%fo = dir(strcat(path, '\', '*.txt')); % Windows User
+fo = dir(strcat(path, '/', '*.txt')); % Mac User
+for kk=1:3
+    
+    %folderlist = dir(strcat(path, '/', 'regexpdr........')); % Mac User
+    path = strcat(cd, '/', 'dr1-fvmh0');
+    folderlist = regexp(fo{kk},'dr.......','match');
 
-fo = dir(strcat(path, '\', '*.txt'));
-
+    %fo = dir(strcat(path, '\', '*.txt')); % Windows User
+    fo = [fo dir(strcat(path, '/', '*.txt'))]; % Mac User
+    kk = kk+1;
+end
