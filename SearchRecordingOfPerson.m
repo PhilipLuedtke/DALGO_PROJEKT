e5a9c2d3b058bc358dd982b7ec6dd61e893751b6
Jan Heimann <jan.heimann@student.jade-hs.de>
2015-04-25 22:04:39 +0200

    Hier die Fkt zur Suche von Aufnahmen einer Person. Ein paar Kommentare fehlen noch...werden noch nachgetragen, sowie eine Testversion.

--- SearchRecordingOfPerson.m
+++ SearchRecordingOfPerson.m
@@ -1,29 +1,78 @@
-function [out_param] = SearchRecordingOfPerson(in_param)
-% function to do something usefull (fill out)
+function [folder_per, rec_list] = SearchRecordingOfPerson(name_per)
+% function to do search the file with the recordings of a testperson
 % Usage [out_param] = SearchRecordingOfPerson(in_param)
 % Input Parameter:
-%	 in_param: 		 Explain the parameter, default values, and units
+%	 per_name: 		 
 % Output Parameter:
-%	 out_param: 	 Explain the parameter, default values, and units
+%	 folder_per:
+%    rec_list:
 %------------------------------------------------------------------------ 
 % Example: Provide example here if applicable (one or two lines) 
 
-% Author: J.Heimann, D.Popken, P.Luedtke (c) TGM @ Jade Hochschule applied licence see EOF 
-% Source: If the function is based on a scientific paper or a web site, 
-%         provide the citation detail here (with equation no. if applicable)  
+% Author: J.Heimann, D.Popken, P.Luedtke (c) TGM @ Jade Hochschule applied 
+% licence see EOF 
+% Source: ---
+%
 % Version History:
-% Ver. 0.01 initial create (empty) 25-Apr-2015  Initials (eg. JB)
+% Ver. 0.01 initial create (empty)    25-Apr-2015     Initials (JH, DP, PL)
+% Ver.  1.0 first implementation      25-Apr-2015     Initials (JH, DP, PL)
 
-%%
+
+
+%% Einage des absoluten Pfades der TIMIT MIT-Datenbank
 
 % absoluter Pfad (Jan)
 path_name = 'C:\Users\Jan Heimann\Documents\MATLAB\Dalgo\DALGO_PROJEKT\TIMIT MIT';
 
 %%
 
+% Einlesen aller File- und Folder- Namen aus der aus der Datenbank 
+% TIMIT MIT
+audio_file = dir(path_name);
+
+% Zaehlindex
+idx = 1;
+
+% Preallokation ...
+folder_list = cell(size(audio_file));
+
+% 
+for file_idx = 1:length(audio_file)
+    
+    file_name = audio_file(file_idx).name;
+    folder_list{file_idx,1} = regexp(file_name,'dr.......','match');
+    
+    if ~isempty(folder_list{file_idx,1})
+        folder(idx,1) = folder_list{file_idx,1};
+        idx = idx+1;
+    end
+    
+end
+
+%%
+
+% Sucht den Ordner der gesuchten Person
+match_per = regexp(folder(:,1), name_per, 'match');
+
+% Gibt die Position aus
+pos =  find(~cellfun('isempty',match_per));
 
 
+folder_per = folder{pos,:};
 
+%% Suche aller Audioaufnahmen im Ordner der gesuchten Person
+
+audio_file = dir(strcat(path_name,'\',folder_per,'\','*wav'));
+
+rec_list = cell(size(audio_file));
+
+for kk = 1:length(audio_file)
+    
+    rec = audio_file(kk).name;
+    
+    rec_list{kk,1} = rec;
+    
+end
 
 
 
